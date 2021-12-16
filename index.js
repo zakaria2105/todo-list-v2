9e05d727ad797ba33159b967b777346502212cf3
zakaria2105 <zelar903@gmail.com>
2021-12-16 23:58:22 +0100

    Connection to mysql

---
+++ index.js
@@ -0,0 +1,16 @@
+var mysql = require('mysql2');
+
+let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'zero',
+    password: '123698745za',
+    database: 'todo_list',
+});
+
+connection.connect(function (err) {
+    if (err) {
+        return console.error('error: ' + err.message);
+    }
+
+    console.log('Connected to the MySQL.');
+});
